038db111f80354ea76069ffedd3c1603c5f1bbb8
Godwin <macqueensawyer@yahoo.com>
2025-01-30 18:50:30 +0100

    build the application

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html",
+        404: "404.html",
+        advertise: "advertise.html",
+        authors: "authors.html",
+        comingSoon: "coming-soon.html",
+        contact: "contact.html",
+        news: "news.html",
+        policy: "privacy-policy.html",
+        terms: "terms-and-conditions.html"
+      }
+    }
+  },
   plugins: [tailwindcss()],
-  base: "/macqueen006"
 });
